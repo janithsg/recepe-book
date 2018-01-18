2ffe07515b2f0bba0983226cceab3ba75929b40b
Chathuranga_Dulaj <chathuranga@avanago.com>
2018-01-18 13:17:26 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,37 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { CoreComponent } from './core/core.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './core/home/home.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    CoreComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    SigninComponent,
+    SignupComponent,
+    HeaderComponent,
+    HomeComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
